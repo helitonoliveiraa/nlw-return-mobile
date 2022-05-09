8c8c9800a92204a209b917efa965f28b43e79f13
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-09 09:43:13 -0300

    :art: chore: setup global font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+
+import Widget from './src/components/Widget';
+import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading  />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar 
+        style="light"
+        backgroundColor='transparent'
+        translucent 
+      />
+
+      <Widget />
     </View>
   );
 }
@@ -13,8 +34,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colors.background,
   },
 });
